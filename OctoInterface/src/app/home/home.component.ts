e7303cec40f9c653d5a700c32d91d1c709bed52b
Andrew Perkins <perkinsandy0@gmail.com>
2020-08-08 18:58:58 -0600

    Worked on css

--- OctoInterface/src/app/home/home.component.ts
+++ OctoInterface/src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-home',
   template: `
     <div class="home">
-      <a class="button" routerLink="/files" class="button">Files</a>
+      <a class="button neo-up" routerLink="/files">Files</a>
     </div>
   `,
   styles: [`
@@ -14,9 +14,6 @@ import { Component, OnInit } from '@angular/core';
         height: 100vh;
       }
       .button {
-        background: #2A2D32;
-        box-shadow: 10px 10px 15px #24262B, -10px -10px 15px #30343A;
-        border-radius: 20px;
         width: 100px;
         height: 50px;
         display: grid;
